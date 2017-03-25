6f323749d8c2f73b8267904b626decb4bffbeeda
Burjal <mingzhihou@163.com>
2017-03-26 01:56:10 +0800

    publish

--- app/src/main/java/com/burjal/maventest/MainActivity.java
+++ app/src/main/java/com/burjal/maventest/MainActivity.java
@@ -2,12 +2,20 @@ package com.burjal.maventest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.burjal.testaar.TestUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView) findViewById(R.id.txt);
+        tvInfo.setText(TestUtils.getInfo());
     }
 }
---
+++ testaar/src/main/java/com/burjal/testaar/TestUtils.java
@@ -0,0 +1,13 @@
+package com.burjal.testaar;
+
+/**
+ * Created by burjal on 2017/3/26.
+ */
+
+public class TestUtils {
+
+    public static String getInfo() {
+        return "From com.burjal.testaar";
+    }
+
+}
